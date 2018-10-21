03507a4ce5774fdba6fcbf102436d2fe1dad5285
nguyhai <tenth11@gmail.com>
2018-10-21 18:36:34 -0500

    Created an example for using async programming

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -10,7 +10,21 @@ namespace AsyncProgramming
     {
         static void Main(string[] args)
         {
-            ConcatenateChars();
+            //Task t = new Task(ConcatenateChars);
+            Task t = Task.Factory.StartNew(ConcatenateChars); // Easier way to do this
+
+            t.Start(); // Starts the method, runs async to our other code. At the same time, it runs our writeline code. The main thread is the high priority, not the method.
+
+
+
+            Console.WriteLine("In Progress"); // You want this line to execute at the same time our method executes. 
+
+
+
+
+
+
+            t.Wait(); // We are telling our task to wait until completion before proceeding. 
             Console.WriteLine("Completed");
         }
 
