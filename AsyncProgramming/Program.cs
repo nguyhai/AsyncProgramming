b6b4d6c036e555e435b28be3ef1b1d568827c038
nguyhai <tenth11@gmail.com>
2018-10-22 00:15:52 -0500

    Refactored Example

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -14,8 +14,9 @@ namespace AsyncProgramming
             char charToConcatenate = '1';
 
             //Task t = new Task(ConcatenateChars);
-            Task<string> t = ConcatenateChars(charToConcatenate, count);
+            Task<string> t = ConcatenateCharsAsync(charToConcatenate, count);
 
+            
 
             Console.WriteLine("In Progress"); // You want this line to execute at the same time our method executes. 
             //t.Wait(); // We are telling our task to wait until completion before proceeding. No longer need this if we use Async/Await
@@ -24,26 +25,31 @@ namespace AsyncProgramming
 
         }
 
-        // Lets create a method that concatenates a really long string
-        public async static Task<string> ConcatenateChars(char charToConcatenate, int count) // Now using async keyword
+        public static string ConcatenateChars(char charToConcatenate, int count)
         {
-            Task<string> t = Task<string>.Factory.StartNew(()=> 
-            {
-                string concatenatedString = string.Empty;
+            string concatenatedString = string.Empty;
 
-                for (int i = 0; i < count; i++)
-                {
-                    concatenatedString += charToConcatenate;
-                }
-                return concatenatedString;
+            for (int i = 0; i < count; i++)
+            {
+                concatenatedString += charToConcatenate;
+            }
+            return concatenatedString;
+        }
+
+        // Lets create a method that concatenates a really long string
+        public async static Task<string> ConcatenateCharsAsync(char charToConcatenate, int count) // Now using async keyword
+        {
+            return await Task<string>.Factory.StartNew(()=> 
+            {
+                return ConcatenateChars(charToConcatenate, count);
             });
 
             // using the await keyword for return. For this example, we are only waiting for the return
             // Since we have the await keyword here, the above Task is executing asynchronously. It is just waiting fro "t" to complete. 
-            string result = await t;
+            //string result = await t;
 
-            Console.WriteLine("Completed");
-            return result; 
+            //Console.WriteLine("Completed");
+            //return result; 
         }
     }
 }
